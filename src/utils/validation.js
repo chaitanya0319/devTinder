d4628efb0f9fb48eb8bc6fca9b2b7115b3138f5a
Chaitanya <Chaitanyabandi18@gmail.com>
2025-12-02 18:53:12 -0600

    logout and update apis

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -16,4 +16,23 @@ const validateSignUpData = (data) => {
     }
 };
 
-module.exports = { validateSignUpData };
+const validateEditProfileData = (req) => {
+    const allowedEditFields = [
+      "firstName",
+      "lastName",
+      "emailId",
+      "photoUrl",
+      "gender",
+      "age",
+      "about",
+      "skills",
+    ];
+  
+    const isEditAllowed = Object.keys(req.body).every((field) =>
+      allowedEditFields.includes(field)
+    );
+  
+    return isEditAllowed;
+  };
+
+module.exports = { validateSignUpData, validateEditProfileData,};
